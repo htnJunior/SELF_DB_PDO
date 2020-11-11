e0b073b353ba7af73a58dc462bc85668f2307bb7
HtnJunior <junioor_souzaa282@hotmail.com>
2020-11-11 17:40:16 -0300

    Utilizando a instancia unica. Utilizando autoload com composer.

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require __DIR__."/vendor/autoload.php";
+
+use Source\Database\Connect;
+
+$pdo = Connect::getInstance();
+var_dump($pdo);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd3dec717cec5afb2f8afe2923b4b9ccc39954765',
+    'name' => 'vendor_name/connect_singleton',
+  ),
+  'versions' => 
+  array (
+    'vendor_name/connect_singleton' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd3dec717cec5afb2f8afe2923b4b9ccc39954765',
+    ),
+  ),
+);
